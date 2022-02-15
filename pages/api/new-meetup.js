e754980eb4853e97fcb3e1f7772f3e2a005de44d
dario-nihil <dario.nihil@gmail.com>
2022-02-15 14:05:06 +0100

    ready for deplyment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://dario:MtJTi%40nPXM26_9%23@cluster0.cywid.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+    res.status(201).json({ message: "Meetup inserted! " });
+  }
+};
+
+export default handler;
